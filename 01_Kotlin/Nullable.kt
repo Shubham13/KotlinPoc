397c95d646a3ac7d8924a80e399dd009fc639ca2
himanshu288m <himanshugupta288@gmail.com>
2021-05-20 19:32:11 +0530

    Profile

--- 01_Kotlin/Nullable.kt
+++
@@ -1,6 +0,0 @@
-fun main(args:Array<String>){
-    var str : String? = "Hello"
-//    var str : String = "Hello"
-    str = null
-    print(str)
-}
--- Intents/app/src/main/java/com/example/intents/Course.kt
+++
@@ -1,8 +0,0 @@
-package com.example.intents
-
-data class Course (
-    val name: String,
-    val teacher: String,
-    val lectures: Int,
-    val fees: Int
-)
